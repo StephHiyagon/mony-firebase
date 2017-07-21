5e6dd32568cfc4424b1bde89e551ca9cfe5cdaae
Angie Condor <angiegita1602@gmail.com>
2017-07-21 07:04:08 -0500

    section 2

--- public/assets/js/componentes/info.js
+++ public/assets/js/componentes/info.js
@@ -3,8 +3,24 @@ const Home = (update) => {
         <h1>TE AYUDAMOS A HACER REALIDAD TU PROYECTO</h1>
         <button type="button" class="btn btn-default">Consigue inversión</button>
     </section>`);
-   // state.screen = UserRegister;
+   const section= $('<<section></section>');
+   
+   section.append(ComoFunciona());
+ 
+
     //update();
 
     return homeContainer;
+}
+
+const ComoFunciona = () => {
+    const funcionamiento = $(`<section id="como-funciona">
+        <h1>¿CÓMO FUNCIONA?</h1>
+        <div class="paso1">Define tu objetivo y cuánto dinero necesitas para lograrlo</div>
+        <span class="glyphicons glyphicons-fees-payments"></span>
+    </section>`);
+   // state.screen = UserRegister;
+    //update();
+
+    return funcionamiento;
 }
