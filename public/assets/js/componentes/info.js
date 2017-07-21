0e322add0501715b648cf681a1263cb233efb018
Erika Vidal <diann852@gmail.com>
2017-07-21 12:32:40 -0500

    seccion funciones

--- public/assets/js/componentes/info.js
+++ public/assets/js/componentes/info.js
@@ -12,22 +12,50 @@ const Home = (update) => {
                 <p>Para poder registrarte en nuestra plataforma debes contar con una empresa establecida, también te recomendamos tener tus datos actualizados en la Sunat</p>
                 <p>Para asegurarte puedes dirigirte al siguiente link:</p>
                  <a href="http://www.sunat.gob.pe/" target="_blank">www.sunat.gob.pe</a>
-                <button type="button" class="btn btn-default btn-close" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-default btn-close" data-dismiss="modal">Cerrar</button>
               </div>
             </div>
           </div>
         </div>
     </section>`);
 
-    const sectionBeneficios = $('<seccion id="beneficios" class="beneficios"></seccion>');
-    const sectionFunciones  = $('<section></section>');
+    const sectionBeneficios = $('<section id="beneficios" class="beneficios"></section>');
+    const sectionFunciones  = $('<section class="funciones"></section>');
 
-    container.append(seccionHome);
-    sectionBeneficios.append(Beneficios());
-    container.append(sectionBeneficios);
     sectionFunciones.append(ComoFunciona());
+    sectionBeneficios.append(Beneficios());
+
+    container.append(sectionHome);
+    container.append(sectionFunciones);
+    container.append(sectionBeneficios);
+
 
     return container;
+}
+const ComoFunciona = _ => {
+    const funcionamiento = $(`<h2 class="text-center">¿CÓMO FUNCIONA MONY?</h2>
+             
+                  <div class="row">
+                      <div class="col-sx-12 col-md-4">
+                            <img class="img-responsive" src="assets/img/paso1.png" alt="logo paso 1">
+                            <h4 class="text-center">Define tus objetivos...</h4>
+                            <p class="">Y la cantidad de inversión que necesita para ello.</p>
+                            <p class="">Aquí sería adecuado colocar tu motivación, tu mayor sueño y qué es lo que buscas con tu proyecto. Con ello aseguras que muchos inversores se motiven a invertir apostando por tu proyecto.</p>
+                        </div>       
+                      <div class="col-xs-12 col-md-4">
+                          <img class="img-responsive" src="assets/img/paso2.png" alt="logo paso 2">
+                          <h4 class="text-center">META...</h4>
+                          <p class="">Llegar al 75% del monto definido en menos de un mes. Recuerda que esto dependerá que tan vendedor sea tu perfil.</p>
+                        </div>
+                     <div class="col-xs-12 col-md-4">
+                          <img class="img-responsive" src="assets/img/paso3.png" alt="logo paso 3">
+                          <h4 class="">Interbank te financia...</h4>
+                          <p class="">Si la inversión en tu proyecto llega al 75%, Interbank te financia el resto con una tasa preferencial.</p>
+                        </div>
+                      </div>
+                  </div>`);
+
+    return funcionamiento;
 }
 
 const Beneficios=_=>{
@@ -35,7 +63,7 @@ const Beneficios=_=>{
     const divImage    = $('<div class="col-sx-12 col-md-6"></div>');
     const imagen      = $('<img class="img-responsive" src="assets/img/emprendedora.jpeg">');
     const informacion = $('<div class="col-sx-12 col-md-6"></div>');
-    const titulo      = $('<h3 class="text-center">Los inversionistas podrán elegir cómo invertir en tu negocio</h3>');
+    const titulo      = $('<h2 class="text-center">Los inversionistas podrán elegir cómo invertir en tu negocio</h2>');
     const divLista    = $('<div class="text-left beneficios__informacion"></div>');
     const lista1      = $('<div class="beneficios__informacion--display"><img class="img-responsive" src="assets/img/002-unchecked.png"><h6>Contribuyen sin recompensa.</h6></div>');
     const lista2      = $('<div class="beneficios__informacion--display"><img class="img-responsive" src="assets/img/002-unchecked.png"><h6>Invierten a cambio de productos o servicios de tu empresa.</h6></div>');
@@ -53,13 +81,4 @@ const Beneficios=_=>{
     return container;
 }
 
-const ComoFunciona = () => {
-    const funcionamiento = $(`<section id="como-funciona">
-        <h1>¿CÓMO FUNCIONA?</h1>
-        <div class="paso1">Define tu objetivo y cuánto dinero necesitas para lograrlo</div>
-        <span class="glyphicons glyphicons-fees-payments"></span>
-    </section>`);
-
-    return funcionamiento;
-}
 
